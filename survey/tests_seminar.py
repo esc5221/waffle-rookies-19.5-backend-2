e457f4316c81ac6a08eec821469c8c18a9438eb7
esc5221 <esc5221@gmail.com>
2021-10-10 21:03:24 +0900

    edited : POST seminar/{id}/user v2

--- survey/tests_seminar.py
+++ survey/tests_seminar.py
@@ -572,6 +572,25 @@ class PostSeminarUser(TestCase):
                                     HTTP_AUTHORIZATION=getattr(self,f"inst_2_token"),
                                     data=request_data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_post_seminar_noMatchingId(self):
+        request_data = {
+            "name" : f"seminar_1",
+            "capacity" : 10,
+            "count" : 5,
+            "time" : "14:30",
+            "online" : "True",
+        }
+
+        request_data = {
+            "role" : "participant",
+        }
+        with transaction.atomic():
+            response = self.client.post(f'/api/v1/seminar/100/user/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"inst_2_token"),
+                                    data=request_data)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_post_seminar_user_withNotAcceptedParticipant(self):
         request_data = {
@@ -732,14 +751,322 @@ class PostSeminarUser(TestCase):
         #print(json.dumps(data, ensure_ascii = False, indent=4))
 
 # DELETE /api/v1/seminar/{seminar_id}/user/
-class DeleteSeminaruser(TestCase):
+class DeleteSeminarUser(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.participant = UserFactory(
-            username='part',
+        for i in range(1,4): 
+            user = UserFactory(
+                username=f"inst_{i}",
+                password='password',
+                first_name='first',
+                last_name='last',
+                email=f'inst_{i}@snu.ac.kr',
+                is_instructor=True
+            )
+            setattr(cls, f"inst_{i}", user)
+            token = 'JWT ' + jwt_token_of(User.objects.get(email=f'inst_{i}@snu.ac.kr'))
+            setattr(cls, f"inst_{i}_token", token)
+
+        for i in range(1,4): 
+            user = UserFactory(
+                username=f"part_{i}",
+                password='password',
+                first_name='first',
+                last_name='last',
+                email=f'part_{i}@snu.ac.kr',
+                is_participant=True
+            )
+            setattr(cls, f"part_{i}", user)
+            token = 'JWT ' + jwt_token_of(User.objects.get(email=f'part_{i}@snu.ac.kr'))
+            setattr(cls, f"part_{i}_token", token)
+
+        user = UserFactory(
+            username=f"partinst",
             password='password',
-            first_name='Davin',
-            last_name='Byeon',
-            email='bdv111@snu.ac.kr',
-            is_participant=True
+            first_name='first',
+            last_name='last',
+            email=f'partinst@snu.ac.kr',
+            is_participant=True,
+            is_instructor=True
         )
+        setattr(cls, f"partinst", user)
+        token = 'JWT ' + jwt_token_of(User.objects.get(email=f'partinst@snu.ac.kr'))
+        setattr(cls, f"partinst_token", token)
+            
+    def test_post_seminar_user_correctRole(self):
+        request_data = {
+            "name" : f"seminar_1",
+            "capacity" : 10,
+            "count" : 5,
+            "time" : "14:30",
+            "online" : "True",
+        }
+
+        with transaction.atomic():
+            response = self.client.post('/api/v1/seminar/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"inst_1_token"),
+                                    data=request_data)
+        data = response.json()
+        seminar_id = data['id']
+
+        request_data = {
+            "role" : "participant",
+        }
+        with transaction.atomic():
+            response = self.client.post(f'/api/v1/seminar/{seminar_id}/user/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"part_1_token"),
+                                    data=request_data)
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        data = response.json()
+        #print(json.dumps(data, ensure_ascii = False, indent=4))
+        self.assertIn("id", data)
+        self.assertEqual(data['name'], f"seminar_1")
+        self.assertEqual(data['capacity'], 10)
+        self.assertEqual(data['count'], 5)
+        self.assertEqual(data['time'], "14:30")
+        self.assertEqual(data['online'], True)
+        
+        participants_data = data.get("participants")[0]
+        self.assertIn("id", participants_data)
+        self.assertEqual(participants_data['username'], f"part_1")
+        self.assertEqual(participants_data['email'], f"part_1@snu.ac.kr")
+        self.assertEqual(participants_data['first_name'], f'first')
+        self.assertEqual(participants_data['last_name'], f'last')
+        self.assertIn("joined_at", participants_data)
+        self.assertEqual(participants_data['is_active'], True)
+        self.assertEqual(participants_data['dropped_at'], None)
+
+        request_data = {
+            "role" : "instructor",
+        }
+        with transaction.atomic():
+            response = self.client.post(f'/api/v1/seminar/{seminar_id}/user/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"inst_2_token"),
+                                    data=request_data)
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        data = response.json()
+        self.assertIn("id", data)
+        self.assertEqual(data['name'], f"seminar_1")
+        self.assertEqual(data['capacity'], 10)
+        self.assertEqual(data['count'], 5)
+        self.assertEqual(data['time'], "14:30")
+        self.assertEqual(data['online'], True)
+        
+        instructors_data = data.get("instructors")[1]
+        self.assertIn("id", instructors_data)
+        self.assertEqual(instructors_data['username'], f"inst_2")
+        self.assertEqual(instructors_data['email'], f"inst_2@snu.ac.kr")
+        self.assertEqual(instructors_data['first_name'], f'first')
+        self.assertEqual(instructors_data['last_name'], f'last')
+        self.assertIn("joined_at", instructors_data)
+
+    def test_post_seminar_user_wrongRole(self):
+        request_data = {
+            "name" : f"seminar_1",
+            "capacity" : 10,
+            "count" : 5,
+            "time" : "14:30",
+            "online" : "True",
+        }
+
+        with transaction.atomic():
+            response = self.client.post('/api/v1/seminar/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"inst_1_token"),
+                                    data=request_data)
+        data = response.json()
+        seminar_id = data['id']
+
+        request_data = {    
+            "role" : "foobar",
+        }
+        with transaction.atomic():
+            response = self.client.post(f'/api/v1/seminar/{seminar_id}/user/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"part_1_token"),
+                                    data=request_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+        request_data = {
+            "role" : "instructor",
+        }
+        with transaction.atomic():
+            response = self.client.post(f'/api/v1/seminar/{seminar_id}/user/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"part_2_token"),
+                                    data=request_data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        request_data = {
+            "role" : "participant",
+        }
+        with transaction.atomic():
+            response = self.client.post(f'/api/v1/seminar/{seminar_id}/user/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"inst_2_token"),
+                                    data=request_data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_post_seminar_user_withNotAcceptedParticipant(self):
+        request_data = {
+            "name" : f"seminar_1",
+            "capacity" : 10,
+            "count" : 5,
+            "time" : "14:30",
+            "online" : "True",
+        }
+
+        with transaction.atomic():
+            response = self.client.post('/api/v1/seminar/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"inst_1_token"),
+                                    data=request_data)
+        data = response.json()
+        seminar_id = data['id']
+
+        self.part_1.participant.accepted = False
+        self.part_1.participant.save()
+        self.part_1.save()
+
+        request_data = {    
+            "role" : "participant",
+        }
+        with transaction.atomic():
+            response = self.client.post(f'/api/v1/seminar/{seminar_id}/user/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"part_1_token"),
+                                    data=request_data)
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_post_seminar_user_fullCapacity(self):
+        request_data = {
+            "name" : f"seminar_1",
+            "capacity" : 2,
+            "count" : 5,
+            "time" : "14:30",
+            "online" : "True",
+        }
+
+        with transaction.atomic():
+            response = self.client.post('/api/v1/seminar/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"inst_1_token"),
+                                    data=request_data)
+        data = response.json()
+        seminar_id = data['id']
+
+        request_data = {
+            "role" : "participant",
+        }
+
+        # capacity is 2, but 3 users are applying
+        for i in range(1,4):
+            with transaction.atomic():
+                response = self.client.post(f'/api/v1/seminar/{seminar_id}/user/', 
+                                        content_type='application/json', 
+                                        HTTP_AUTHORIZATION=getattr(self,f"part_{i}_token"),
+                                        data=request_data)
+            #print("part count : ", Seminar.objects.get(id=seminar_id).participant_count)
+        
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        data = response.json()
+    
+    def test_post_seminar_user_alreadyInstructor(self):
+        request_data = {
+            "name" : f"seminar_1",
+            "capacity" : 10,
+            "count" : 5,
+            "time" : "14:30",
+            "online" : "True",
+        }
+        with transaction.atomic():
+            response = self.client.post('/api/v1/seminar/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"inst_1_token"),
+                                    data=request_data)
+        request_data = {
+            "name" : f"seminar_2",
+            "capacity" : 10,
+            "count" : 5,
+            "time" : "14:30",
+            "online" : "True",
+        }
+        with transaction.atomic():
+            response = self.client.post('/api/v1/seminar/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"inst_2_token"),
+                                    data=request_data)
+        data = response.json()
+        seminar_id = data['id']
+
+        request_data = {
+            "role" : "instructor",
+        }
+        # inst_1 is applying another seminar
+        with transaction.atomic():
+            response = self.client.post(f'/api/v1/seminar/{seminar_id}/user/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"inst_1_token"),
+                                    data=request_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        data = response.json()
+        #print(json.dumps(data, ensure_ascii = False, indent=4))
+
+    def test_post_seminar_user_alreadyInSeminar(self):
+        request_data = {
+            "name" : f"seminar_1",
+            "capacity" : 10,
+            "count" : 5,
+            "time" : "14:30",
+            "online" : "True",
+        }
+        with transaction.atomic():
+            response = self.client.post('/api/v1/seminar/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"partinst_token"),
+                                    data=request_data)
+        data = response.json()
+        seminar_id = data['id']
+
+        request_data = {
+            "role" : "instructor",
+        }
+        # partinst is applying his/her seminar as an instructor
+        with transaction.atomic():
+            response = self.client.post(f'/api/v1/seminar/{seminar_id}/user/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"partinst_token"),
+                                    data=request_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        request_data = {
+            "role" : "participant",
+        }
+        # partinst is applying his/her seminar as a participant
+        with transaction.atomic():
+            response = self.client.post(f'/api/v1/seminar/{seminar_id}/user/', 
+                                    content_type='application/json', 
+                                    HTTP_AUTHORIZATION=getattr(self,f"partinst_token"),
+                                    data=request_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        request_data = {
+            "role" : "participant",
+        }
+        # double applying as a participant
+        for i in range(2):
+            with transaction.atomic():
+                response = self.client.post(f'/api/v1/seminar/{seminar_id}/user/', 
+                                        content_type='application/json', 
+                                        HTTP_AUTHORIZATION=getattr(self,f"part_1_token"),
+                                        data=request_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        #print(json.dumps(data, ensure_ascii = False, indent=4))
